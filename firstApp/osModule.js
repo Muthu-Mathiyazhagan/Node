82b51d774782af3bad527556618349817c8d321d
Muthu Mathiyazhagan <muthumathiyazhagan@gmail.com>
2021-08-24 18:42:38 +0530

    ES 6 Feature implemented for log function that Concatnate without using + Operator

--- firstApp/osModule.js
+++ firstApp/osModule.js
@@ -7,5 +7,6 @@ var upTime = os.uptime();
 console.log("Total Memory : " + totalMemory);
 console.log("Free Memory : " + freeMemory);
 console.log("Up Time in Minutes : " + upTime / 60);
+console.log(`Up Time is : ${upTime}`);
 
 module.exports.totalMemory = os;
