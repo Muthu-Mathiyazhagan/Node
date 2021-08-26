6b2c0e4d7a566fee0f8732f9b0ba77fad1f89a0f
Muthu Mathiyazhagan <muthumathiyazhagan@gmail.com>
2021-08-26 17:46:38 +0530

    Reading Route Parameter's OBJECT using 'req.params'

--- express-demo/index.js
+++ express-demo/index.js
@@ -9,6 +9,15 @@ app.get('/api/courses/:id', (req, res) => {
     res.send(req.params.id);
 });
 
+//  Below GET Request read ROUTE Parameter OBJECT and Return the Same
+//  request : http://localhost:3000/api/post/2018/04/29
+//  response: {"year":"2018","month":"04","date":"29"}
+app.get('/api/post/:year/:month/:date', (req, res) => {
+
+    res.send(req.params);
+});
+
+
 
 
 const port = process.env.PORT || 3000;
