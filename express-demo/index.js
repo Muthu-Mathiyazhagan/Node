112b09878f21a04db520f68a52f986cd3ec7b52d
Muthu Mathiyazhagan <muthumathiyazhagan@gmail.com>
2021-08-26 17:45:42 +0530

    Reading Route Parameter using 'req.params.id'

--- express-demo/index.js
+++ express-demo/index.js
@@ -3,5 +3,13 @@ const express = require('express'); // It will return a function
 app.get('/', (req, res) => {
     res.send('Hello world.! using Node Monitor');
 });
+
+//  Below GET Request read route Parameter and Return the Same
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port # ${port}...`));
