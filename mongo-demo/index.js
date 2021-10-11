b0b9615726e42535b4f32fc75936b9ef1bbcb229
Muthu Mathiyazhagan <muthumathiyazhagan@gmail.com>
2021-10-11 15:46:31 +0530

    Connecting To mongoDB, Schemas, Models, Querying and Sorting

---
+++ mongo-demo/index.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost/playground')
+    .then(() => console.log("Connected to MongoDB...!"))
+    .catch((err) => console.error("Error In Connecting to Database", err))
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+
+});
+const Course = mongoose.model('Course', courseSchema);
+
+
+async function createCourse() {
+    const course = new Course({
+        name: "React JS",
+        author: "Muthu",
+        tags: ['React', 'frontend'],
+        isPublished: true
+    });
+
+    const result = await course.save();
+    console.log(result);
+}
+
+createCourse();
+
+
+
